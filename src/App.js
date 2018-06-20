238fb7e0aca379952c0c3eb2b43fa4a708a546f0
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2018-06-20 22:54:51 +1000

    project part 1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo6 from './logo6.png';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo6} width="200" height="102" className="" alt="logo" />
           <h1 className="App-title">Welcome to Digital-Tech</h1>
         </header>
         <p className="App-intro">
