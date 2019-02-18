5bcf1ba5e930e791964868dcd40c4fc8e89f9626
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-02-18 22:08:47 +0800

    fix security issues in dependencies

---
+++ build/precache-manifest.0de3efcadcd17939967abe63d1369839.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd57bc73580e66f27ef1",
+    "url": "/React-Project/static/css/main.695d4ced.chunk.css"
+  },
+  {
+    "revision": "dd57bc73580e66f27ef1",
+    "url": "/React-Project/static/js/main.dd57bc73.chunk.js"
+  },
+  {
+    "revision": "d56c0760aaa9228ed847",
+    "url": "/React-Project/static/js/runtime~main.d56c0760.js"
+  },
+  {
+    "revision": "41246040b21bbeb25289",
+    "url": "/React-Project/static/js/2.41246040.chunk.js"
+  },
+  {
+    "revision": "c743bc7f9a6fb021e3434614cac9a686",
+    "url": "/React-Project/static/media/rendered2.c743bc7f.jpg"
+  },
+  {
+    "revision": "e839967aff9333fae3df34ed2a74816c",
+    "url": "/React-Project/static/media/logo_1.e839967a.png"
+  },
+  {
+    "revision": "5e9b33abf2afaeb4963860245c127435",
+    "url": "/React-Project/static/media/logo_2.5e9b33ab.png"
+  },
+  {
+    "revision": "a24ed12df6373c60145e16cd3731728a",
+    "url": "/React-Project/static/media/optimized2.a24ed12d.jpg"
+  },
+  {
+    "revision": "f57e24d4bba3b3833d1483192ceb1f0f",
+    "url": "/React-Project/index.html"
+  }
+];
